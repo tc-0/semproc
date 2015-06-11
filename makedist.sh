f080872d24d131f3bf090af331635ee0ed84c9b2
Tobias Columbus <tobias.columbus@kit.edu>
2015-06-11 13:35:48 +0200

    initial commit v0.1

---
+++ makedist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p texmf/tex/latex/semproc
+mkdir -p texmf/doc/latex/semproc
+
+cp semproc.pdf texmf/doc/latex/semproc
+cp doc/example.tex texmf/doc/latex/semproc
+cp doc/example.bib texmf/doc/latex/semproc
+cp semproc.cls texmf/tex/latex/semproc
+cp semproc.dtx texmf/tex/latex/semproc
+(
+  cd texmf
+  pwd
+  zip ../semproc.zip tex/latex/semproc/* doc/latex/semproc/*
+)
